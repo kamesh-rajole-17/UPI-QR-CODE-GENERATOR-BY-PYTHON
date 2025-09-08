37431d6cbf5d1cc9d4c5f84e82d486b8566eaa23
kamesh-rajole-17 <rajolekamesh@gmail.com>
2025-09-08 15:56:00 +0530

    Add files via upload
    UPI (Unified Payments Interface) is a real-time payment system widely used in India. A UPI QR Code allows users to make payments simply by scanning the code with a UPI-enabled mobile app. In Python, we can generate such QR codes using the qrcode library.
    
    Objective :-
    
    To create a Python program that generates a QR code for UPI payments, containing payment details like UPI ID, name, and amount. When scanned, it redirects to the UPI payment app for quick transactions.
    
    Requirements
    
    Python 3
    
    Libraries:
    
    qrcode → to generate QR codes
    PIL (Pillow) → to handle image output

---
+++ qrCode.py
@@ -0,0 +1,25 @@
+import qrcode
+
+# Taking upi id as a input
+upi_id = input("Enter your UPI ID = ")
+
+phonepe_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+# Create QR Codes for each payment app
+
+phonepe_qr = qrcode.make(phonepe_url)
+paytm_qr = qrcode.make(paytm_url)
+google_pay_qr = qrcode.make(google_pay_url)
+
+# Save as image file 
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+# display the QR Code (You may need to install PIP install Library)
+
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
